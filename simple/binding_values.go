31b81e09fd31ce36d109d767a4bc78a40d33d775
JalalAlbarn96 <jalal.albarn@gmail.com>
2022-10-20 05:08:57 +0700

    Binding Values

---
+++ simple/binding_values.go
@@ -0,0 +1,10 @@
+package simple
+
+// Binding Values
+var Avalue = &A{}
+var Bvalue = &B{}
+
+// func BindingValuesInjector() *AdanB {
+// 	wire.Build(wire.Value(a), wire.Value(b), wire.Struct(new(AdanB), "*"))
+// 	return nil
+// }
